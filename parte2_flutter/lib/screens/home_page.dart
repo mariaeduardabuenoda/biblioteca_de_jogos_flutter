b7171c3d434f38a77507b290271dfcae22133322
Maria Bueno <maria.bueno.silva17@escola.pr.gov.br>
2026-09-11 12:53:14 -0300

    Criacao do exercicio 07

--- parte2_flutter/lib/screens/home_page.dart
+++ parte2_flutter/lib/screens/home_page.dart
@@ -1,35 +1,72 @@
 import 'package:flutter/material.dart';
 import '../models/jogo.dart';
-import '../../widgets/jogo_card.dart';
+import '../models/biblioteca.dart';
+import '../widgets/jogo_card.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    // Total obtido no exercício 4
-    const int totalJogos = 2;
+    // Biblioteca que agrupa os jogos
+    final biblioteca = Biblioteca(
+      nome: 'Minha Biblioteca',
+      jogos: [
+        Jogo(
+          titulo: 'Minecraft',
+          plataforma: 'PC',
+          genero: 'Sandbox',
+          anoLancamento: 2011,
+          preco: 99.90,
+          dataCadastro: DateTime(2026, 9, 1),
+        ),
 
-    //Objetos da biblioteca 
-    final List<Jogo> jogos = [
-      Jogo( 
-        titulo: 'Minecraft',
-        plataforma: 'PC',
-        genero: 'Sandbox',
-        anoLancamento: 2011,
-        preco: 99.90,
-        dataCadastro: DateTime(2026, 9, 1),
-      ),
+        Jogo(
+          titulo: 'The Legend of Zelda',
+          plataforma: 'Nintendo Switch',
+          genero: 'Aventura',
+          anoLancamento: 2017,
+          preco: 199.90,
+          dataCadastro: DateTime(2026, 9, 1),
+        ),
 
-      Jogo( 
-        titulo: 'The Legend of Zelda',
-        plataforma: 'Nintendo Switch',
-        genero: 'Aventura',
-        anoLancamento: 2017,
-        preco: 199.90,
-        dataCadastro: DateTime(2026, 9, 1),
-      )
-    ];
+        Jogo(
+          titulo: 'Mortal Kombat 1',
+          plataforma: 'PS5',
+          genero: 'Luta',
+          anoLancamento: 2023,
+          preco: 299.90,
+          dataCadastro: DateTime(2026, 9, 1),
+        ),
+
+        Jogo(
+          titulo: 'Fortnite',
+          plataforma: 'PC',
+          genero: 'Battle Royale',
+          anoLancamento: 2017,
+          preco: 0.0,
+          dataCadastro: DateTime(2026, 9, 1),
+        ),
+
+        Jogo(
+          titulo: 'God of War',
+          plataforma: 'PS5',
+          genero: 'Ação',
+          anoLancamento: 2018,
+          preco: 249.90,
+          dataCadastro: DateTime(2026, 9, 1),
+        ),
+
+        Jogo(
+          titulo: 'Super Mario Odyssey',
+          plataforma: 'Nintendo Switch',
+          genero: 'Plataforma',
+          anoLancamento: 2017,
+          preco: 299.90,
+          dataCadastro: DateTime(2026, 9, 1),
+        ),
+      ],
+    );
 
     return Scaffold(
       appBar: AppBar(
@@ -43,11 +80,11 @@ class HomePage extends StatelessWidget {
         mainAxisAlignment: MainAxisAlignment.start,
         crossAxisAlignment: CrossAxisAlignment.center,
         children: [
-          // Total do exercício 4
+          // Total de jogos da Biblioteca
           Padding(
             padding: const EdgeInsets.all(16.0),
             child: Text(
-              'Total de jogos: $totalJogos',
+              'Total de jogos: ${biblioteca.totalJogos}',
               style: const TextStyle(
                 fontSize: 22,
                 fontWeight: FontWeight.bold,
@@ -55,14 +92,13 @@ class HomePage extends StatelessWidget {
             ),
           ),
 
-        
-          // Lista do exercício 7
+          // Lista dinâmica do exercício 7
           Expanded(
             child: ListView.builder(
-              itemCount: jogos.length,
+              itemCount: biblioteca.jogos.length,
               itemBuilder: (context, index) {
                 return JogoCard(
-                  jogo: jogos[index],
+                  jogo: biblioteca.jogos[index],
                 );
               },
             ),
